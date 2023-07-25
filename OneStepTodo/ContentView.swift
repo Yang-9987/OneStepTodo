694f8f82bdc97f36098c2a9ad965a36ec7a8c09f
Yang-9987 <1013404703@qq.com>
2023-07-25 23:17:47 +0800

    Initial Commit

---
+++ OneStepTodo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OneStepTodo
+//
+//  Created by Yang Jianqi on 2023/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OneStepTodo/OneStepTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneStepTodoApp.swift
+//  OneStepTodo
+//
+//  Created by Yang Jianqi on 2023/7/25.
+//
+
+import SwiftUI
+
+@main
+struct OneStepTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
